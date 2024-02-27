f21cafe2fece64f2ff9a70f45fda4e62bcbbd67b
DIMITRIS LAMPROU <dimitris2001lamprou@gmail.com>
2024-02-27 20:17:33 +0200

    diari8misi ton folders

---
+++ Assets/Scenes/Battle Scenes/Scripts/Mode.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Mode : MonoBehaviour
+{
+    public string[] modeNames = {"for demo map 1", "with deck 1 vs enemy A", "with deck 2 vs enemy A" };
+    public string mode = "for demo map 1";
+}
