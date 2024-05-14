9b5b39d11751a23c8bd95d7c8273c0e06f3e284e
DIMITRIS LAMPROU <dimitris2001lamprou@gmail.com>
2024-05-14 12:27:03 +0300

    update hero stats

--- Assets/Scenes/Battle Scene/Scripts/NonMonoBehaviour/Hero.cs
+++ Assets/Scenes/Battle Scene/Scripts/NonMonoBehaviour/Hero.cs
@@ -8,8 +8,10 @@ public static class Hero
     public static int exp = 0;
     public static int handLimit = 5;
     public static int stamina = 3;
-    //public static int staminaCap = 3; For later
+    public static int staminaCap = 3;
     public static int scales = 0;
+    public static int level = 1;
+    public static int inventorySlots = 1;
 
     public static int attack;
     public static int addDefence;
